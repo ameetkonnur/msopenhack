d383cba9a5fdde2f303d15db6b7ec64fe1534ad2
Ameet Konnur <ameetk@microsoft.com>
2018-09-20 14:59:17 +0530

    6

--- processOrder/index.js
+++ processOrder/index.js
@@ -1,13 +1,13 @@
 module.exports = async function (context, myBlob) {
-    context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    //context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
     var file_name = context.bindingData.name;
     var file_prefix = file_name.substring(0,14);
     context.log ('prefix : ' + file_prefix);
 
     const azure = require('azure-storage');
-    context.log(process.env.msopenhackchallenge6_STORAGE);
+    //context.log(process.env.msopenhackchallenge6_STORAGE);
     const blobService = azure.createBlobService(process.env.msopenhackchallenge6_STORAGE);
-    
+    context.log(blobService);
     blobService.listBlobsSegmentedWithPrefix('challenge6blob',file_prefix,null,(err,data)=>{
         if (err) {
             context.log(err);
