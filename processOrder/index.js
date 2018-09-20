fc49b1821041764a51fcc3cd506e5915337573ac
Ameet Konnur <ameetk@microsoft.com>
2018-09-20 14:03:08 +0530

    order process

--- processOrder/index.js
+++ processOrder/index.js
@@ -1,3 +1,6 @@
 module.exports = async function (context, myBlob) {
     context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
+    var file_name = context.bindingData.name;
+    var file_prefix = file_name.substring(0,13);
+    context.log ('prefix' + file_prefix);
 };
