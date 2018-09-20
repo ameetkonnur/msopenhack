2b9a4293468354f2429956ad963c91456aef1eed
Ameet Konnur <ameetk@microsoft.com>
2018-09-20 15:05:27 +0530

    6

--- processOrder/index.js
+++ processOrder/index.js
@@ -1,7 +1,7 @@
 module.exports = async function (context, myBlob) {
     //context.log("JavaScript blob trigger function processed blob \n Name:", context.bindingData.name, "\n Blob Size:", myBlob.length, "Bytes");
     var file_name = context.bindingData.name;
-    var file_prefix = file_name.substring(1,14);
+    var file_prefix = file_name.substring(0,14);
     context.log ('prefix : ' + file_prefix);
 
     const azure = require('azure-storage');
