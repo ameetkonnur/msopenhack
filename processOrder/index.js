efe54032d9093a69ee216604481b922ad8ef46fa
Ameet Konnur <ameetk@microsoft.com>
2018-09-20 14:38:32 +0530

    6

--- processOrder/index.js
+++ processOrder/index.js
@@ -7,18 +7,12 @@ module.exports = async function (context, myBlob) {
     const azure = require('azure-storage');
     const blobService = azure.createBlobService(process.env.msopenhackchallenge6_STORAGE);
     
-    const list = () => {
-        return new Promise((resolve,reject)=>{
-            blobService.listBlobsSegmentedWithPrefix('challenge6blob',file_prefix,null,(err,data)=>{
-                if (err) {
-                    context.log(err);
-                }
-                else {
-                    context.log ('Blobs in container with prefix ' + file_prefix + ' ' + data);
-                }    
-            })
-        })
-    }
-
-    list();
+    blobService.listBlobsSegmentedWithPrefix('challenge6blob',file_prefix,null,(err,data)=>{
+        if (err) {
+            context.log(err);
+        }
+        else {
+            context.log ('Blobs in container with prefix ' + file_prefix + ' ' + data);
+        }    
+    })
 };
